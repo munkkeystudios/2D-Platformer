7d0055cdbd8ac885d65f99a9d2e2c90c67575022
Muhammad Talha <26100221@lums.edu.pk>
2024-06-24 09:05:32 +0500

    comments added to health scripts

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -7,11 +7,11 @@ public class Health : MonoBehaviour
     public float maxHealth; //allow only geetting but not setting
     [SerializeField]private float currentHealth;
 
-    public delegate void HealthChanged(float currentHealth);
-    public event HealthChanged OnHealthChanged;
+    public delegate void HealthChanged(float currentHealth);//delegate for health change event (similar to function pointer in c++ )
+    public event HealthChanged OnHealthChanged;// event for health change based on delegate, will notify the subscriber methods when health changes.
 
-    public delegate void Died();
-    public event Died OnDied;
+    public delegate void Died();//similarly a delegate for died event
+    public event Died OnDied;//for death notifications
     void Awake()
     {
         currentHealth = maxHealth;
@@ -31,15 +31,15 @@ public class Health : MonoBehaviour
     }
     public void TakeDamage(float damage)
     {
-        if (currentHealth - damage <= 0)
+        if (currentHealth - damage <= 0)//checking if the damage results in death
         {
-            currentHealth = 0;
-            OnHealthChanged?.Invoke(currentHealth);
-            OnDied?.Invoke();
+            currentHealth = 0;//setting health to 0
+            OnHealthChanged?.Invoke(currentHealth);//notifying subscriber about the health change
+            OnDied?.Invoke();//notifying subscriber about the death
         }
         else
         {
-            currentHealth -= damage;
+            currentHealth -= damage;//reducing health and notifying subscriber
             OnHealthChanged?.Invoke(currentHealth);
         }
     }
@@ -47,7 +47,7 @@ public class Health : MonoBehaviour
     public void Heal(float healAmount)
     {
 
-       if (currentHealth + healAmount >= maxHealth)
+       if (currentHealth + healAmount >= maxHealth)//same for healing, changes health and notifies subscriber, making sure to not go above max health
        {
            currentHealth = maxHealth;
            OnHealthChanged?.Invoke(currentHealth);
--- Assets/Scripts/Health/HealthBarUpdater.cs
+++ Assets/Scripts/Health/HealthBarUpdater.cs
@@ -5,18 +5,18 @@ using UnityEngine.UI;
 
 public class HealthBarUpdater : MonoBehaviour
 {
-    public Health healthComponent;
+    public Health healthComponent;//the player or enemy whose health bar is to be updated
     public Image healthBar;
 
-    private void OnEnable()
+    private void OnEnable()//called when gameobject created
     {
-        healthComponent.OnHealthChanged += UpdateHealthBar;
-        healthComponent.OnDied += HandleDeath;
+        healthComponent.OnHealthChanged += UpdateHealthBar;//subscribing to the health change event of the healthComponent, meaning UpdateHealthBar will be called everytime health changes
+        healthComponent.OnDied += HandleDeath;//same for death calling handledeath
     }
 
-    private void OnDisable()
+    private void OnDisable()//called when gameobject destroyed
     {
-        healthComponent.OnHealthChanged -= UpdateHealthBar;
+        healthComponent.OnHealthChanged -= UpdateHealthBar;//unsubscribe to prevent memory leaks like for pointers in c++
         healthComponent.OnDied -= HandleDeath;
     }
 
@@ -24,12 +24,12 @@ public class HealthBarUpdater : MonoBehaviour
     {
         if (healthComponent.maxHealth > 0)
         {
-            healthBar.fillAmount = currentHealth / healthComponent.maxHealth;
+            healthBar.fillAmount = currentHealth / healthComponent.maxHealth;//calculating fill amount of health bar based on current and maximum health
         }
     }
 
-    private void HandleDeath()
+    private void HandleDeath()//handle death
     {
-        Destroy(gameObject);
+        Destroy(gameObject);//currently just destroying the gameobject to be replaced with other logic
     }
 }
