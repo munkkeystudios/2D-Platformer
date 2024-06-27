9654023008d8aef5181d1a89959f5e7830b60999
Muhammad Talha <26100221@lums.edu.pk>
2024-06-27 15:29:46 +0500

    cleaned up prototype 1

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -3,6 +3,59 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
+
+    public AttackDefinitions[] attacks;
+    private float[] lastAttackTimes;
+
+    private void Awake()
+    {
+        lastAttackTimes = new float[attacks.Length];
+        foreach (var attack in attacks)
+        {
+            if (attack.attackCollider != null)
+            {
+                attack.attackCollider.SetActive(false);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            StartCoroutine(ActivateAttackCollider(0));
+        }
+        else if (Input.GetKeyDown(KeyCode.X))
+        {
+            StartCoroutine(ActivateAttackCollider(1));
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            StartCoroutine(ActivateAttackCollider(2));
+        }
+    }
+
+    IEnumerator ActivateAttackCollider(int attackIndex)
+    {
+        if (Time.time - lastAttackTimes[attackIndex] < attacks[attackIndex].cooldownTime)
+        {
+            yield break;
+        }
+
+        GameObject collider = attacks[attackIndex].attackCollider;
+        if (collider != null)
+        {
+            collider.SetActive(true);
+            yield return new WaitForSeconds(0.8f);
+            collider.SetActive(false);
+        }
+        lastAttackTimes[attackIndex] = Time.time;
+    }
+}
+    /*
+     ATTEMPT AT DOING ALL THREE ATTACKS, wasn't working so currently implemented only 1 attack for first draft
+
+
     public float damage = 10f;
     public float attackRange = 1.5f;
     private float attackTimer;
@@ -39,57 +92,4 @@ public class PlayerAttack : MonoBehaviour
                 enemyH.TakeDamage(damage);
             }
         }
-
-    }
-    /*
-     ATTEMPT AT DOING ALL THREE ATTACKS, wasn't working so currently implemented only 1 attack for first draft
-    public AttackDefinitions[] attacks;
-    private float[] lastAttackTimes;
-
-    private void Awake()
-    {
-        lastAttackTimes = new float[attacks.Length];
-        foreach (var attack in attacks)
-        {
-            if (attack.attackCollider!= null)
-            {
-                attack.attackCollider.SetActive(false);
-            }
-        }
-    }
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            StartCoroutine(ActivateAttackCollider(0));
-        }
-        else if (Input.GetKeyDown(KeyCode.X))
-        {
-            StartCoroutine(ActivateAttackCollider(1));
-        }
-        else if(Input.GetKeyDown(KeyCode.C))
-        {
-            StartCoroutine(ActivateAttackCollider(2));
-        }
-    }
-
-    IEnumerator ActivateAttackCollider(int attackIndex)
-    {
-        if (Time.time -lastAttackTimes[attackIndex] <attacks[attackIndex].cooldownTime)
-        {
-            yield break;
-        }
-
-        GameObject collider = attacks[attackIndex].attackCollider;
-        if (collider !=null)
-        {   collider.SetActive(true);
-            yield return new WaitForSeconds(0.8f);
-            collider.SetActive(false);
-        }
-        lastAttackTimes[attackIndex] = Time.time;
-    }
-
     */
-}
-    
