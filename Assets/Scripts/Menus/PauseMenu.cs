402ec0c10db6be8646cff8092a7ef9fbb6ad309a
ahmad-malik12 <159530108+ahmad-malik12@users.noreply.github.com>
2024-07-07 22:49:03 +0500

    l

---
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu ;
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+    
+    public void Home()
+    {
+        SceneManager.LoadSceneAsync(0);
+        Time.timeScale = 1;
+
+    }
+    public void Restart()
+    {
+        SceneManager.LoadSceneAsync(1);
+        Time.timeScale = 1;
+
+    }
+    public void Options()
+    {
+
+    }
+}
